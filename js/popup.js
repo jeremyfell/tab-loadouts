a5692a0f248b48374f00501b8e87b5da93710ed0
jeremyfell <jeremyafell@gmail.com>
2018-01-07 20:14:08 -0800

    Added functionality to create all tabs in the current window for a tab loadout

--- js/popup.js
+++ js/popup.js
@@ -1,5 +1,16 @@
 HOTKEY_LOADOUTS = [
-  1, null, 3, 4, null, null, null, null, null, null
+  {
+    name: "testing",
+    hotkey: 1,
+    links: [
+      "https://www.w3schools.com/jsref/met_win_prompt.asp",
+      "https://www.reddit.com/r/mealtimevideos/",
+      "https://developer.chrome.com/extensions/tabs#method-create",
+      "https://www.reddit.com/r/videos/",
+    ]
+  }
+
+  , null, 3, 4, null, null, null, null, null, null
 ]
 
 function disableUnusedLoadoutNumbers() {
@@ -14,23 +25,42 @@ function addLoadoutButtonClickEvents() {
 
   for (var i = 0; i < 10; i++) {
     loadoutButtons[i].addEventListener("click", function() {
-      openLoadout(this.id[this.id.length - 1]);
+      openLoadout(parseInt(this.id[this.id.length - 1]));
     });
   }
 }
 
 function openLoadout(loadoutNumber) {
   if (document.getElementById("open-loadout-" + String(loadoutNumber)).getAttribute("disabled")) return;
-  alert(loadoutNumber);
-  currentLoadout = HOTKEY_LOADOUTS[loadoutNumber];
+  currentLoadoutLinks = HOTKEY_LOADOUTS[(loadoutNumber + 9) % 10].links;
+  if (currentLoadoutLinks.length === 0) return;
 
-  // for (var i = 0; i < currentLoadout.length; i++) {
+  chrome.tabs.getAllInWindow(null, function(tabs) {
+    var tabIds = []
 
-  // }
+    // Removes all tabs in the current window except for the first tab
+    if (tabs.length > 1) {
+
+      for (var i = 1; i < tabs.length; i++) {
+        tabIds.push(tabs[i].id);
+      }
+
+      chrome.tabs.remove(tabIds);
+    }
+
+    // Updates the first tab, and creates new tabs, to create the tab loadout
+    chrome.tabs.update(tabs[0].id, {url: currentLoadoutLinks[0]})
+
+    for (var i = 1; i < currentLoadoutLinks.length; i++) {
+      chrome.tabs.create({url: currentLoadoutLinks[i]});
+    }
+
+  });
+
 }
 
-function openSettings() {
 
+function openSettings() {
 }
 
 
@@ -46,12 +76,16 @@ function highlightButton(loadoutNumber) {
 
 function getLoadoutNumberFromKeyPress(e) {
   if (e.which >= 48 && e.which <= 57) {
+    // Key pressed was 0-9
     return e.which - 48;
   } else if (e.which >= 96 && e.which <= 105) {
+    // Key pressed was 0-9 on the numpad
     return e.which - 96
   } else if (e.which === 189) {
+    // Key pressed was '-', which is a shortcut for settings
     return 10;
   } else {
+    // Some other key was pressed
     return -1;
   }
 }
@@ -67,7 +101,6 @@ document.addEventListener("keyup", function(e) {
 
   loadoutNumber = getLoadoutNumberFromKeyPress(e);
   if (loadoutNumber === -1) return;
-
   (loadoutNumber === 10) ? openSettings() : openLoadout(loadoutNumber);
 
 });
