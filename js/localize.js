f728e779f77be6f33f8fcc57c8aa7f50db7a55b7
jfell <jfell@sfu.ca>
2020-12-03 19:14:53 -0800

    Fixed issues with localization

--- js/localize.js
+++ js/localize.js
@@ -1,10 +1,9 @@
 function localize() {
-  var page = document.getElementsByTagName('html')[0];
-  var oldPage = page.innerHTML.toString();
-
-  var newPage = oldPage.replace(/__MSG_(\w+)__/g, function(match, key) {
-    return key ? chrome.i18n.getMessage(key) : "";
+  document.querySelectorAll('[title]').forEach(element => {
+    element.title = chrome.i18n.getMessage(element.title)
   });
 
-  if (oldPage !== newPage) page.innerHTML = newPage;
+  document.querySelectorAll('[placeholder]').forEach(element => {
+    element.placeholder = chrome.i18n.getMessage(element.placeholder)
+  });
 }
